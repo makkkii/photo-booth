5f7f07c2ce4642f3d26254fd265fbfc99fc2930c
Mohd Makki <mohd.makki.sh@gmail.com>
2019-04-28 22:50:10 +0530

    Added common components.

--- app/Main.js
+++ app/Main.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native';
 
+import Button from './components/common/Button'
+
 export default class Main extends Component {
   render() {
     return (
       <View>
         <Text>I'm Main</Text>
+        <Button textButton='Submit' />
       </View>
     )
   }

529f5d0c51426071f2021010df1142061b5fa66c
Mohd Makki <mohd.makki.sh@gmail.com>
2019-04-28 20:42:29 +0530

    Initial app setup.

---
+++ App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import firebase from 'firebase';
+
+import Main from './app/Main';
+import { config } from './firebase-cred';
+
+export default class App extends React.Component {
+  componentWillMount() {
+    // Initialize Firebase
+    firebase.initializeApp(config);
+  }
+
+  render() {
+    return (
+      <Main />
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
